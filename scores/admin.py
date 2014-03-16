db53752ac533cc3a649ee7170eddba47f2b6ea53
Elena Williams <github@elena.net.au>
2014-03-16 19:11:28 +0800

    Added scores admin for Event and Person, with related Poll/EventPoll as inlines.

--- scores/admin.py
+++ scores/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from scores.models import Event, Person, EventPoll, Poll
 
-# Register your models here.
+
+class EventPollInline(admin.TabularInline):
+    model = EventPoll
+    extra = 0
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [EventPollInline]
+
+admin.site.register(Event, EventAdmin)
+
+
+class PersonPollInline(admin.TabularInline):
+    model = Poll
+    extra = 0
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [PersonPollInline]
+
+admin.site.register(Person, PersonAdmin)
