06114d3dbbf9a3507c6ae599380b2d409f4a5306
Elena Williams <github@elena.net.au>
2014-03-16 15:02:05 +0800

    First pass of score models (without results).

--- scores/models.py
+++ scores/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+POLL_CHOICES = (
+    ('bodies', 'bodies'),
+    ('boobies', 'boobies'),
+)
+
+
+class Event(models.Model):
+    date = None
+    current = models.BooleanField(default=False)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class EventPoll(models.Model):
+    event = models.ForeignKey('scores.Event')
+    poll = models.CharField(max_length=256, choices=POLL_CHOICES)
+    value = models.IntegerField()
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Poll(models.Model):
+
+    event = models.ForeignKey('scores.Event')
+    person = models.ForeignKey('scores.Person')
+    poll = models.CharField(max_length=256, choices=POLL_CHOICES)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return "[{0}] {1} -- {2}: {3}".format(self.event, self.person, self.poll, self.value)
