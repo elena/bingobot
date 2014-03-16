7818d394684723091145f2921ac9f0c542e52955
Elena Williams <github@elena.net.au>
2014-03-16 13:55:37 +0800

    Fixed local db env settings. rm stupid emacs files ended up in repo.

--- bingobot/settings.py
+++ bingobot/settings.py
@@ -70,7 +70,16 @@ SITE_ID = 1
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
+
+if bool(os.environ.get('LOCAL_DEV', False)):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
